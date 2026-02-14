58a132f1a7c40e13ffff81e5f88d1ab8d68a6a82
vaahi1001 <vaahika@gmail.com>
2026-02-14 17:31:41 +0530

    Update streamlit_app.py

--- test/streamlit_app.py
+++ test/streamlit_app.py
@@ -3,6 +3,11 @@ import joblib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+
 
 st.title("End-to-End Regression Demo with Regularization")
 
