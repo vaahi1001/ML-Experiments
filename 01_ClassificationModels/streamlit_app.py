c79e7b0fa71b5f146b458448bf61cbdbacfdfc55
vaahi1001 <vaahika@gmail.com>
2026-02-14 18:06:58 +0530

    Update streamlit_app.py

--- 01_ClassificationModels/streamlit_app.py
+++ 01_ClassificationModels/streamlit_app.py
@@ -7,6 +7,7 @@ import pandas as pd
 st.write("Hello World")  # Works inside Streamlit app
 print("Hello World")     # Works in terminal, but might not show in Streamlit UI
 
-pipeline = joblib.load("pipeline = joblib.load("/mount/src/ml-experiments/01_ClassificationModels/01_logisticreg_pipeline_1.pkl")
+pipeline = joblib.load("/mount/src/ml-experiments/01_ClassificationModels/01_logisticreg_pipeline_1.pkl")
+
 
 st.title("Heart Disease Prediction App")
